264a197c5227f811e9cf949b615ad5d5917e1fb8
Carly Sierra <carlylwomack@gmail.com>
2018-04-12 18:40:17 -0500

    added local variables

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ exports.DATABASE_URL =
     global.DATABASE_URL ||
     'mongodb://localhost/foodprepdb';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'secret string';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
-exports.CLIENT_ORIGIN = 'https://gallant-swanson-133f3e.netlify.com'; 
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
